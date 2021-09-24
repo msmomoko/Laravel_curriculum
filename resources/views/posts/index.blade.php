1a3bed3a4a501e91e04028f2467c295851c33170
msmomoko <b1920590@planet.kanazawa-it.ac.jp>
2021-09-24 18:58:50 +0900

    ブログ投稿一覧の表示

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title> Blog </title> 
+    </head>
+    <body>
+        <h1>Blog </h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+    　  </div>
+    </body>
+</html>
